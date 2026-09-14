55dae4a9d1c40572b5a7de31ee6765ec93284593
Gladiatorsarius <pmitze@gmx.de>
2026-09-14 18:08:33 +0200

    feat(BasicDiscordBot.py): Added Startup flow fixed restart_helper task and added getting team member ids
    changed the startup flow to send a team dm to all team members of the bot or just the owner if the bot doesnt belong to a team. fixed helper task to seerestart.txt and shutdown.txt in the main dictonary and not in his own dictonary. Added Team member id getting

--- Main.py
+++ Main.py
@@ -7,10 +7,9 @@ from discord.ext import commands
 
 dotenv.load_dotenv()
 
-Testing = os.getenv("Testing")
+testing = os.getenv("testing")
 
 Dev_Guild_ID =int(os.getenv("Dev_Guild_ID"))
-Developer_ID = int(os.getenv("Developer_ID"))
 Original_Source_Code_URL = "https://example.com" #Please do not change this URL. It is used to provide credit to the original author of the bot.
 Original_Author_ID = 1130514544960225402 #Please do not change this id. It is used to provide credit to the original author of the bot.
 Original_Author_Name = "Gladiatorsarius" #Please do not change this name. It is used to provide credit to the original author of the bot.
@@ -24,7 +23,7 @@ client = commands.Bot(intents=intents, command_prefix="!")
 
 @client.event
 async def setup_hook():
-    await client.add_cog(BasicDiscordBot(client, developer_id=Developer_ID , dev_guild_id=Dev_Guild_ID))
+    await client.add_cog(BasicDiscordBot(client,  dev_guild_id=Dev_Guild_ID , testing=testing))
 
 client.run(token, log_handler=handler, log_level=logging.DEBUG)
 
--- src/basicdiscordbot/BasicDiscordBot.py
+++ src/basicdiscordbot/BasicDiscordBot.py
@@ -5,13 +5,13 @@ from types import SimpleNamespace
 from . import git_commands
 import subprocess
 from discord import app_commands
+from pathlib import Path
 
 class BasicDiscordBot(commands.Cog):
     def __init__(
         self,
         client,
         dev_guild_id: int= None,
-        developer_id: int = None,
         original_source_code_url: str = None,
         original_author_id: int = None,
         original_author_name: str = None,
@@ -21,7 +21,6 @@ class BasicDiscordBot(commands.Cog):
         self.client = client
 
         self.Dev_Guild_ID = dev_guild_id
-        self.Developer_ID = developer_id
     
         self.Original_Source_Code_URL = original_source_code_url
         self.Original_Author_ID = original_author_id
@@ -31,21 +30,14 @@ class BasicDiscordBot(commands.Cog):
         self.testing = testing
 
 
-    def check_developer_id(self, user_id: int) -> bool:
-        return user_id == self.Developer_ID
-
-    def is_developer(self):
-        async def predicate(interaction: discord.Interaction):
-            return self.check_developer_id(interaction.user.id)
-        return app_commands.check(predicate)
-
     async def cog_load(self):
         try:
+            dev_guild = await self.client.fetch_guild(self.Dev_Guild_ID) if self.Dev_Guild_ID is not None else None
             if not self.testing:
                 synced_Global = await self.client.tree.sync()
                 if self.Dev_Guild_ID is not None:
                     synced_Guild = await self.client.tree.sync(guild=discord.Object(id=self.Dev_Guild_ID))
-                    print(f"Synced {len(synced_Global)} global commands and {len(synced_Guild)} guild commands.")
+                    print(f"Synced {len(synced_Global)} global commands and {len(synced_Guild)} to {dev_guild.name}.")
                 else:
                     print(f"Synced {len(synced_Global)} global commands.")
             else:
@@ -53,25 +45,92 @@ class BasicDiscordBot(commands.Cog):
                     raise ValueError("In testing mode, you must provide a Dev_Guild_ID to sync commands to a specific guild.")
                 self.client.tree.copy_global_to(guild=discord.Object(id=self.Dev_Guild_ID))
                 synced_guild = await self.client.tree.sync(guild=discord.Object(id=self.Dev_Guild_ID))
-                print(f"Synced {len(synced_guild)} commands to the guild {self.Dev_Guild_ID}.")
+                print(f"Synced {len(synced_guild)} commands to {dev_guild.name}.")
                 self.client.tree.clear_commands(guild=None)
                 await self.client.tree.sync()
                 print("Cleared global commands.")
         except Exception as e:
             print(f"Error syncing commands: {e}")
+        await self.get_Team_members()
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f'Logged in as {self.client.user.name}')
-        if self.Developer_ID is not None and self.Version is not None:
-            developer_user = await self.client.fetch_user(self.Developer_ID)
-            if developer_user:
-                await developer_user.send(f"Bot Started Successfully. Version: {self.Version}")
-        elif self.Developer_ID is not None:
-            developer_user = await self.client.fetch_user(self.Developer_ID)
-            if developer_user:
-                await developer_user.send(f"Bot Started Successfully.")
-        elif self.Version is not None:
-            print(f"Bot Started Successfully. Version: {self.Version}")
+        if not self.testing:
+            if self.Version is not None:
+                await self.send_team_dm(f"Bot Started Successfully. Version: {self.Version}")
+                print(f'Logged in as {self.client.user.name}. Version: {self.Version }')
+            elif self.Version is None:
+                await self.send_team_dm(f"Bot Started Successfully.")
+                print(f'Logged in as {self.client.user.name}')
+            if not self.update_git.is_running():
+                self.update_git.start()
         else:
-            print(f"Bot Started Successfully.")
+            if not self.restart_helper.is_running():
+                self.restart_helper.start()
+            print(f'Logged in as {self.client.user.name} in testing mode.')
+
+
+    def check_team_member(self, user_id: int) -> bool:
+        return user_id in self.team_member_ids
+
+    def is_team_member(self):
+        async def predicate(self, interaction: discord.Interaction):
+            return self.check_team_member(interaction.user.id)
+        return app_commands.check(predicate)
+
+    async def send_team_dm(self, message: str):
+        for member_id in self.team_member_ids:
+            member = await self.client.fetch_user(member_id)
+            if member:
+                try:
+                    await member.send(message)
+                except Exception as e:
+                    print(f"Failed to send DM to {member.name}: {e}")
+
+
+
+    async def get_Team_members(self):
+        self.team_member_ids = []
+        info = await self.client.application_info()
+        if info.team is not None:
+            team_members = info.team.members
+            for member in team_members:
+                self.team_member_ids.append(member.id)
+        else:
+            owner = info.owner
+            self.team_member_ids.append(owner.id)
+
+    
+    @tasks.loop(seconds=1)
+    async def restart_helper(self):
+        if self.testing:
+            base_dir = Path.cwd()
+            shutdown_file = base_dir / "shutdown.txt"
+            restart_file = base_dir / "restart.txt"
+            startup_file = base_dir / "startup.txt"
+
+            if shutdown_file.exists():
+                print("Shutdown signal received. Shutting down...")
+                shutdown_file.unlink()
+                await self.client.close()
+                return
+
+            if restart_file.exists():
+                print("Restart signal received. Restarting...")
+                restart_file.unlink()
+                startup_file.touch()
+                await self.client.close()
+
+    @restart_helper.before_loop
+    async def before_restart_helper(self):
+        if not self.testing:
+            self.restart_helper.stop()
+        await self.client.wait_until_ready()
+
+    @tasks.loop(minutes=30)
+    async def update_git(self):
+        pass
+
+
+    #else:
+
